a15417378592b7a5a2e35c668160c50bc1e89f5b
Apu Sutra Dhar <apusutradhar77@gmail.com>
2023-05-02 09:44:32 +0600

    sent data /chefs path

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const cors = require("cors");
-app.use(cors());
 const chefs = require("./chefs.json");
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
