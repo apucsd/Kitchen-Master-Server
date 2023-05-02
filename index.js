e22d9abf1d94f0d2a8707c7c535acc22e9bcf9a0
Apu Sutra Dhar <apusutradhar77@gmail.com>
2023-05-02 09:40:09 +0600

    added cores

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const cors = require("cors");
+app.use(cors());
 
-const chefs = require("./chefs.json");
 app.get("/", (req, res) => {
   res.send("Hello World!");
-});
-app.get("/chefs", (req, res) => {
-  res.send(chefs);
 });
 
 app.listen(port, () => {
